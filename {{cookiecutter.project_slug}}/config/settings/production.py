856ebbd657915df644bf52841d3605db9cb9cce1
Toni Milovan <tmilovan@fwd.hr>
2017-08-11 10:35:41 +0200

    production file fix

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -334,7 +334,7 @@ LOGGING = {
     }
 }
 # {% endif %}
-# Custom Admin URL, use {% url 'admin:index' %}
+# Custom Admin URL, use {% raw %}{% url 'admin:index' %}{% endraw %}
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
